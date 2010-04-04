84a7f6eddc697fce32657a4e675deb3351fd7bdc
ben@benatkin.com <ben@benatkin.com>
2010-04-03 22:47:53 -0600

    got concept visible to geeks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var gotTweets = false;
+var gotRetweets = false;
+
+function handleTweets(data) {
+  $('#spinner').addClass('invisible');
+	$('#tweetData').visualize({'type': 'area', 'width': '600px', 'height': '350px'});
+}
+
+function getTweets() {
+  gotTweets = gotRetweets = false;
+
+  var username = encodeURIComponent($('input[name=username]').val());
+  if (username == "") { alert("Please enter a username"); $('input[name=username]').focus(); return; }
+
+  $('#spinner').removeClass('invisible');
+  var tweeturl = "http://twitter.com/status/user_timeline/#{user}.json?count=200&callback=?";
+  $.getJSON(tweeturl.replace("#{user}", username), handleTweets);
+  $('#go').blur();
+}
+
+$(function() {
+  $('#go').click(getTweets);
+});
