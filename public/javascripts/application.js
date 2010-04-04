d4fd5ad22c645dc22595e2d797dc9e7c5f093bb4
ben@benatkin.com <ben@benatkin.com>
2010-04-03 23:13:27 -0600

    prettied it up and added cookies

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,33 @@
-var gotTweets = false;
-var gotRetweets = false;
+function loadCookies() {
+  $('input[name=username]').val($.cookie('username'));
+  $('input[name=maxtweets]').val($.cookie('maxtweets'));
+  if ($('input[name=maxtweets]').val() == "")
+    $('input[name=maxtweets]').val('10');
+}
+
+function saveCookies() {
+  $.cookie('username', $('input[name=username]').val());
+  $.cookie('maxtweets', $('input[name=maxtweets]').val());
+}
 
 function handleTweets(data) {
   $('#spinner').addClass('invisible');
-	$('#tweetData').visualize({'type': 'area', 'width': '600px', 'height': '350px'});
+	$('#tweetData').visualize({'type': 'area', 'width': '600px', 'height': '350px'}, $('#graph'));
 }
 
-function getTweets() {
-  gotTweets = gotRetweets = false;
+function getTweets(evt, noValidationErrors) {
+  saveCookies();
 
   var username = encodeURIComponent($('input[name=username]').val());
-  if (username == "") { alert("Please enter a username"); $('input[name=username]').focus(); return; }
+  if (username == "") {
+    if (! noValidationErrors) {
+      alert("Please enter a username");
+    }
+    $('input[name=username]').focus();
+    return;
+  }
 
+  $('#graph').html('');
   $('#spinner').removeClass('invisible');
   var tweeturl = "http://twitter.com/status/user_timeline/#{user}.json?count=200&callback=?";
   $.getJSON(tweeturl.replace("#{user}", username), handleTweets);
@@ -19,5 +35,7 @@ function getTweets() {
 }
 
 $(function() {
+  loadCookies();
+  getTweets(null, true);
   $('#go').click(getTweets);
 });
